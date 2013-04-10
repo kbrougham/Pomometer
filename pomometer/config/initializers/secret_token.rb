e2102fe7d925e545ad284f7c9dbb5773ee67886e
broughamk <brougham.kyle@gmail.com>
2013-04-10 12:30:01 -0400

    first commit of pomometer

---
+++ pomometer/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pomometer::Application.config.secret_token = '1797f8d2014fe114f79224df1fb6c7f81eea0a53760af94a47d708b6b68b5c1ea84bc1bc84499c07d50ecf844161a92f7b13d70cef12898d0aee65b3771cf030'
