12d72d70d39093728d40a810163fbe89a6db4ffe
broughamk <brougham.kyle@gmail.com>
2013-04-11 16:38:34 -0400

    Added alphabetical sorting to task list

--- pomometer/app/controllers/tasks_controller.rb
+++ pomometer/app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   # GET /tasks
   # GET /tasks.json
   def index
-    @tasks = Task.all
+    @tasks = Task.find(:all, :order => "name collate NOCASE")
 
     respond_to do |format|
       format.html # index.html.erb
