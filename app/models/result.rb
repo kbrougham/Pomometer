8ba2db203619bf979c3cd6db4cbc30331e04a8f1
broughamk <brougham.kyle@gmail.com>
2013-04-17 15:42:17 -0400

    EST Display and modifies what will be saved to database for EST

--- app/models/result.rb
+++ app/models/result.rb
@@ -6,7 +6,7 @@ class Result < ActiveRecord::Base
   validate :end_date_after_start_date
 
   def end_date_after_start_date
-  	if started_at > ended_at
+  	if started_at >= ended_at
 		errors.add(:ended_at, " must be after Started At.")
 	end 
   end
