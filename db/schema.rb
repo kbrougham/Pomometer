9b6fbe4d83c2812dc3539ad9a26cd9e5dc2bf47d
Ian Manley <ian.manley11@gmail.com>
2013-04-30 09:31:22 -0400

    new migration changed password name

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130429171644) do
+ActiveRecord::Schema.define(:version => 20130430132902) do
 
   create_table "admins", :force => true do |t|
     t.string   "username"
-    t.string   "password"
+    t.string   "password_digest"
     t.string   "role"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
   create_table "milestones", :force => true do |t|
